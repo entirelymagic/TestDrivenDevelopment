70681ebecd1a690583fff9f958c5424f85987396
entirelymagic@gmail.com <@lexandr@123>
2021-01-29 13:13:08 +0200

    added Checkout addDiscount, addItemPrice, addItem and calculateTotal.

---
+++ CheckOutProject/Checkout.py
@@ -0,0 +1,19 @@
+class Checkout:
+    def __init__(self):
+        self.prices = {}
+        self.total = 0
+
+    def addDiscount(self, item, number_of_items, price):
+        pass
+
+    def addItemPrice(self, item, price):
+        self.prices[item] = price
+
+    def addItem(self, item):
+        self.total += self.prices[item]
+
+    def calculateTotal(self):
+        return self.total
+
+
+
---
+++ CheckOutProject/test_Checkout.py
@@ -0,0 +1,31 @@
+import pytest
+from Checkout import Checkout
+
+
+class TestClassCheckout:
+
+    @pytest.fixture()
+    def checkout(self):
+        checkout = Checkout()
+        checkout.addItemPrice("a", 1)
+        checkout.addItemPrice("b", 2)
+        return checkout
+
+    def test_can_calculate_total(self, checkout):
+        checkout.addItem("a")
+        assert checkout.calculateTotal() == 1
+
+    def test_get_correct_total_with_multiple_items(self, checkout):
+        checkout.addItem("a")
+        checkout.addItem("b")
+        assert checkout.calculateTotal() == 3
+
+    def test_can_add_discount_rule(self, checkout):
+        checkout.addDiscount("a", 3, 2)
+
+    def test_canApplyDiscountRule(self, checkout):
+        checkout.addDiscount("a", 3, 2)
+        checkout.addItem("a")
+        checkout.addItem("a")
+        checkout.addItem("a")
+        assert checkout.calculateTotal() == 2
