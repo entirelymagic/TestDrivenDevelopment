86ff9dbe8932a5ce3cb8afb479d0c0a7868f2ea3
entirelymagic@gmail.com <@lexandr@123>
2021-01-29 12:25:19 +0200

    Added a test for pytest.fixture params

---
+++ test_examples/test_fixture_params.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.fixture(params=[1, 2, 3])
+def setup(request):
+    retVal = request.param
+    print(f"\nSetup! retval={retVal}")
+    return retVal
+
+
+def test1(setup):
+    print(f"\nsetup = {setup}")
+    assert True
